3e9fb39dafcc2d6f7495e896b305253255d87348
jamesthomaswang <hello@jamesthomaswang.com>
2020-10-04 11:08:51 -0700

    Simplified data management and created choropleth plot
    I removed the data manager / region dichotomy in the OOP structure. It
    still feels overcomplicated so I might do one final re-write.
    
    I also managed to figure out how to create an interactive choropleth
    plot, and have a callback function during a click event. The next
    step is to turn this procedure into an object.

--- src/data_management.py
+++ src/data_management.py
@@ -1,10 +1,7 @@
-import functools
-from abc import ABC, abstractmethod
-import pandas as pd
-dr"""These classes read data and order it into a region tree.
+"""These classes read data and order it into a region tree.
 
 There are two categories of classes in this module: region classes and data
-manager classes. The idea is that a tree of "regions" (countries, states, and
+manager classes. The idea is that a tree of "regions" (country, states, and
 counties), as identified by their FIPS code (ignoring country codes) is lazily
 built. Each region instance contains just the data from that region, which is
 pulled from the DataManager object. The structure, therefore, looks like this:
@@ -36,96 +33,118 @@ pulled from the DataManager object. The structure, therefore, looks like this:
     print(pennsylvania.subregion("42003")().head())
 """
 
+import pandas as pd
+from abc import ABC, abstractmethod
+import functools
 
-class Region(ABC):
+
+class Level(ABC):
+    LOWER_LEVEL_CLASS = None
+
     @property
     @abstractmethod
-    def subregion_class(self):
-        pass
+    def DATA_FILE_PATH(self): pass
 
-    def __init__(self, fips, superregion):
-        self._fips = fips
-        self._superregion = superregion
+    def __init__(self, upper_level):
+        self.upper_level = upper_level
 
-        self.data_manager = self._superregion.data_manager
-        self._data = self.data_manager(self).query("fips == @fips")
-
-    @abstractmethod
-    def is_valid_subregion_fips(self, subregion_fips):
-        raise NotImplementedError
-
-    @functools.lru_cache
-    def subregion(self, subregion_fips):
-        if not self.is_valid_subregion_fips(subregion_fips):
-            raise ValueError("{} does not belong to a subregion of {} instance"
-                             " {}.".format(subregion_fips,
-                                           type(self).__name__, self._fips))
-        return self.subregion_class(subregion_fips, self)
+        self.is_uppermost_level = self.upper_level is None
+        self.is_lowermost_level = self.LOWER_LEVEL_CLASS is None
 
     def __call__(self):
-        return self._data
+        return self.data
 
+    @functools.cached_property
+    def lower_level(self):
+        if self.is_lowermost_level:
+            raise ValueError("This level is the lowest level.")
+        return (self.LOWER_LEVEL_CLASS)(self)
 
-class County(Region):
-    subregion_class = None
+    @functools.cached_property
+    def data(self):
+        return pd.read_csv(self.DATA_FILE_PATH,
+                           parse_dates=["date"],
+                           dtype={"fips": pd.StringDtype()})
 
-    def is_valid_subregion_fips(self, subregion_fips):
-        return False
+    @functools.lru_cache
+    def date(self, date):
+        return self()[self()["date"] == date]
 
-    def subregion(self, subregion_fips):
-        raise ValueError("Counties do not have subregions.")
+    @functools.lru_cache(maxsize=50)
+    def set_region(self, fips):
+        self.region = self.Region(self, fips)
+        return self.region
 
+    class Region():
+        def __init__(self, outer, fips=None):
+            self.outer = outer
+            self.fips = fips
 
-class State(Region):
-    subregion_class = County
+            if self.outer.is_uppermost_level:
+                self.data = self.outer()
+                self.subregion_data = self.outer.lower_level()
+                return
 
-    def is_valid_subregion_fips(self, subregion_fips):
-        return (len(subregion_fips) == 5 and
-                subregion_fips.startswith(self._fips))
+            self.data = self.outer()[self.outer()["fips"] == self.fips]
 
+            if self.data.empty:
+                raise ValueError(
+                    "{} is not a valid FIPS code.".format(self.fips))
 
-class Country(Region):
-    subregion_class = State
+            if self.outer.is_lowermost_level:
+                self.subregion_data = None
+                return
 
-    def __init__(self, data_manager):
-        self._fips = 0
-        self._superregion = None
-
-        self.data_manager = data_manager
-        self._data = self.data_manager(self)
-
-    def is_valid_subregion_fips(self, subregion_fips):
-        return len(subregion_fips) == 2
-
-
-class DataManager():
-    class RegionDataManager():
-        def __init__(self, filepath):
-            self._df = pd.read_csv(filepath)
+            self.subregion_data = self.outer.lower_level()[
+                self.outer.lower_level()["fips"].str.startswith(self.fips)
+            ]
 
         def __call__(self):
-            return self._df
+            return self.data
+
+        def subregions(self):
+            return self.subregion_data
+
+        def date(self, date):
+            return self.subregion_data[self.subregion_data['date'] == date]
+
+
+class County(Level):
+    DATA_FILE_PATH = "../data/us-counties.csv"
+
+
+class State(Level):
+    LOWER_LEVEL_CLASS = County
+    DATA_FILE_PATH = "../data/us-states.csv"
+
+
+class Country(Level):
+    LOWER_LEVEL_CLASS = State
+    DATA_FILE_PATH = "../data/us.csv"
 
     def __init__(self):
-        self.region_filepaths = {Country: "../data/us.csv",
-                                 State: "../data/us-states.csv",
-                                 County: "../data/us-counties.csv"}
-        self.region_datas = {region_class: self.RegionDataManager(filepath)
-                             for (region_class, filepath)
-                             in self.region_filepaths.items()}
-
-    def __call__(self, region):
-        if not isinstance(region, type):
-            region = type(region)
-        return self.region_datas[region]()
+        super().__init__(None)
+        self.set_region(0)
 
 
 """
-data_manager = DataManager()
-country = Country(data_manager)
-pennsylvania = country.subregion("42")
-allegheny = pennsylvania.subregion("42003")
+country = Country()  # Note that country.region is automatically set
 print(country().head())
-print(pennsylvania().head())
-print(allegheny().head())
-"""
+print(country.region().head())
+print(country.region.subregions().head())
+print(country.region.date("2020-09-27").head())
+
+states = country.lower_level
+print(states().head())
+states.set_region("06")
+print(states.region().head())
+states.set_region("42")
+print(states.region().head())
+print(states.region.subregions().head())
+print(states.region.date("2020-09-27").head())
+
+counties = states.lower_level
+print(counties().head())
+counties.set_region("42003")
+print(counties.region().head())
+# """
